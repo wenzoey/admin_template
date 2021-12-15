649849773e6a7f6a446a96e21b5f8d70752f325c
wenzoey <wenzoey67@163.com>
2021-12-15 18:40:06 +0800

    暂存

---
+++ front-end/src/pages/User/Login/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return(
+    <img src='https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*P0S-QIRUbsUAAAAAAAAAAABkARQnAQ'></img>
+  )
+};
+
+export default Login;
